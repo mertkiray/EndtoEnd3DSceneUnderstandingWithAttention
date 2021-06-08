c8408f6e6d2c71518688a3de068dfc0888ed9739
Mert Kiray <mkiray@ku.edu.tr>
2021-06-08 17:28:50 +0200

    att gcnn code and config file

--- models/total3d/modules/__init__.py
+++ models/total3d/modules/__init__.py
@@ -1,7 +1,8 @@
+from .att_gcnn import ATGCNN
 from .network import TOTAL3D
 from .layout_estimation import PoseNet
 from .object_detection import Bdb3DNet
 from .mesh_reconstruction import DensTMNet
 from .gcnn import GCNN
 
-__all__ = ['TOTAL3D', 'PoseNet', 'Bdb3DNet', 'DensTMNet', 'GCNN']
+__all__ = ['TOTAL3D', 'PoseNet', 'Bdb3DNet', 'DensTMNet', 'GCNN', 'ATGCNN']
